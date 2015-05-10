9fab1b3132ff54acbbcc134288459e371bebcba8
Archigo <aspo-ja@hotmail.com>
2015-05-10 21:18:56 +0200

    initial

---
+++ Prophets/Prophets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prophets.Startup))]
+namespace Prophets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
